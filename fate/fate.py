b325f1ebcb66e30fbf55258cd38a7275958b2b5e
Honk <throwaway0000ff@gmail.com>
2022-07-05 02:20:55 -0600

    changes discord.message.author to ctx.author

--- fate/fate.py
+++ fate/fate.py
@@ -15,13 +15,13 @@ class fate(commands.Cog):
         """Displays the sheet if there's no text, creates/replaces a sheet for the player if a message is provided."""
 
         if sheetJSON == "":
-            if exists(discord.message.author + "Sheet.txt"):
+            if exists(ctx.author + "Sheet.txt"):
                 await ctx.send("You don't have a sheet, numb nuts! Create one by doing `!fate sheet [json]`")
             else:
-                sheet = open("data/" + discord.message.author + "Sheet.txt", "r")
+                sheet = open("data/" + ctx.author + "Sheet.txt", "r")
                 await ctx.send(sheet.read())
         else:
-            sheet = open("data/" + discord.message.author + "Sheet.txt", "w")
+            sheet = open("data/" + ctx.author + "Sheet.txt", "w")
             sheet.write(sheetJSON)
 
-            await ctx.send(open("data/" + discord.message.author + "Sheet.txt", "r"))
+            await ctx.send(open("data/" + ctx.author + "Sheet.txt", "r"))
