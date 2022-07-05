049e01b8071d6ff1cfd88cf166a83bdeb91f6d31
Honk <throwaway0000ff@gmail.com>
2022-07-05 02:33:50 -0600

    debugging code added to see the flow

--- fate/fate.py
+++ fate/fate.py
@@ -10,20 +10,27 @@ class fate(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def sheet(self, ctx, sheetJSON: Union[discord.Attachment, str]):
-        """Displays the sheet if there's no text, creates/replaces a sheet for the player if a message is provided."""
+        @commands.command()
+        async def sheet(self, ctx, sheetJSON: Union[discord.Attachment, str]):
+            """Displays the sheet if there's no text, creates/replaces a sheet for the player if a message is provided."""
 
-        user = str(ctx.author)
+            user = str(ctx.author)
 
-        if sheetJSON == "":
-            if exists(user + "Sheet.txt"):
-                await ctx.send("You don't have a sheet, numb nuts! Create one by doing `!fate sheet [json]`")
+            if sheetJSON == "":
+                if exists(user + "Sheet.txt"):
+                    print("3")
+                    await ctx.send("You don't have a sheet, numb nuts! Create one by doing `!fate sheet [json]`")
+                else:
+                    print("3")
+                    sheet = open("data/" + user + "Sheet.txt", "r")
+                    await ctx.send(sheet.read())
             else:
-                sheet = open("data/" + user + "Sheet.txt", "r")
-                await ctx.send(sheet.read())
-        else:
-            sheet = open("data/" + user + "Sheet.txt", "w")
-            sheet.write(sheetJSON)
+                if exists(user + "Sheet.txt"):
+                    print("1")
+                    await ctx.send("You don't have a sheet, numb nuts! Create one by doing `!fate sheet [json]`")
+                else:
+                    print("2")
+                    sheet = open("data/" + user + "Sheet.txt", "w")
+                    sheet.write(sheetJSON)
 
-            await ctx.send(open("data/" + user + "Sheet.txt", "r"))
+                    await ctx.send(open("data/" + user + "Sheet.txt", "r"))
